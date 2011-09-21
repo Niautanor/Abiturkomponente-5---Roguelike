57ee32a5141e3e1e1c63dc13d9358a385c399b8b
Tim Malte Graefje <Tim.Graefje@gmx.de>
2011-09-21 18:00:34 +0200

    Commented Tile.cpp for better Documentation

--- Tile.cpp
+++ Tile.cpp
@@ -38,21 +38,21 @@ void Tile::SetBg(CColor Color)
 
 bool Tile::DrawTile(SDL_Surface* pDisplay, TTF_Font* pFont, Uint32 XOffset, Uint32 YOffset, Uint32 TileWidth, Uint32 TileHeight)
 {
-	SDL_Surface* pGlyph = TTF_RenderGlyph_Shaded(pFont, C, fg, bg);
+	SDL_Surface* pGlyph = TTF_RenderGlyph_Shaded(pFont, C, fg, bg); //Zeichnet das Zeichen des Tiles auf pGlyph
 	if(!pGlyph)
 		return false;
 
 	SDL_Surface* pTile = SDL_CreateRGBSurface(SDL_HWSURFACE, TileWidth, TileHeight, 8, pGlyph->format->Rmask, pGlyph->format->Gmask, pGlyph->format->Bmask, pGlyph->format->Amask);
 	if(!pTile)
 		return false;
-	SDL_SetColors(pTile, pGlyph->format->palette->colors, 0, pGlyph->format->palette->ncolors);
+	SDL_SetColors(pTile, pGlyph->format->palette->colors, 0, pGlyph->format->palette->ncolors);//Die Farben von pGlyph importieren
 	SDL_FillRect(pTile, NULL, 0);//0 ist die Hintergrundfarbe von pGlyph
 
 	SDL_Rect TileRc;
 	TileRc.x = (TileWidth / 2) - (pGlyph->w / 2);
 	TileRc.y = TileHeight - pGlyph->h;
 
-	SDL_BlitSurface(pGlyph, NULL, pTile, &TileRc);
+	SDL_BlitSurface(pGlyph, NULL, pTile, &TileRc);//Die Oberfläche pTile enthält jetzt den Glyph mit füllender Hintergrundfarbe
 
 	SDL_FreeSurface(pGlyph);
 
@@ -60,7 +60,7 @@ bool Tile::DrawTile(SDL_Surface* pDisplay, TTF_Font* pFont, Uint32 XOffset, Uint
 	DisplayRc.x = XOffset;
 	DisplayRc.y = YOffset;
 
-	SDL_BlitSurface(pTile, NULL, pDisplay, &DisplayRc);
+	SDL_BlitSurface(pTile, NULL, pDisplay, &DisplayRc);//pTile befindet sich jetzt auf dem Bildschirm
 
 	SDL_FreeSurface(pTile);
 
