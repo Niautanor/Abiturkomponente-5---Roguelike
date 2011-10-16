d2c09fbde535e8a3d10aca762f7bafe4c831c2f0
Tim Malte Graefje <Tim.Graefje@gmx.de>
2011-10-16 20:12:15 +0200

    Added Flagsets
    FlagSet.h
    FlagSet.cpp.inc
    
    Template Based Flagsets

---
+++ FlagSet.h
@@ -0,0 +1,24 @@
+/*
+ * FlagSet.h
+ *
+ *  Created on: 16.10.2011
+ *      Author: Tim
+ */
+
+#pragma once/* FLAGSET_H_ */
+
+template<class T> class FlagSet
+{
+private:
+	T Flags;
+
+public:
+	FlagSet();
+
+	bool Is_Set(T Flag);
+
+	void Set(T Flag);
+	void Unset(T Flag);
+};
+
+#include "FlagSet.cpp.inc"
